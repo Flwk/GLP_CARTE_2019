03053893abb0204299828befc0ce1f7a6e9aedf5
Flwk <bako.blal@gmail.com>
2019-02-04 17:23:51 +0100

    Update

--- GLP_CARTE/src/carte/NormalCard.java
+++ GLP_CARTE/src/carte/NormalCard.java
@@ -1,47 +1,31 @@
 package carte;
-/**
- * @author bsadiki
- */
-public class Card {
+
+public class NormalCard extends Card {
 
 	private String name;
 	private Integer key;
 	private String picturePath;
-	
-	public Card(String name, Integer key, String picturePath) {
-		this.name = name;
-		this.key = key;
-		this.picturePath = picturePath;
+	public NormalCard(String name, Integer key, String picturePath) {
+		super(name, key, picturePath);
+		// TODO Auto-generated constructor stub
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public Integer getKey() {
 		return key;
 	}
-
 	public void setKey(Integer key) {
 		this.key = key;
 	}
-
 	public String getPicturePath() {
 		return picturePath;
 	}
-
 	public void setPicturePath(String picturePath) {
 		this.picturePath = picturePath;
-	}
-
-	@Override
-	public String toString() {
-		return "Card [name=" + name + ", key=" + key + ", picturePath=" + picturePath + ", getName()=" + getName()
-				+ ", getKey()=" + getKey() + ", getPicturePath()=" + getPicturePath() + "]";
 	}
 
 }
